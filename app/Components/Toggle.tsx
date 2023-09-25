f62e0e0fc41daf05687388ed4c01016ce9bf0577
Eduardo Kenji Pageu Hirose <dudu.hirose@hotmail.com>
2023-09-25 20:15:46 -0300

    mudanca de UI para os botoes dos temas dark/light

--- app/Components/Toggle.tsx
+++ app/Components/Toggle.tsx
@@ -31,7 +31,7 @@ const Toggle = () => {
       <div
         className={`peer ${isChecked ? 'after:transition-all after:duration-500 after:left-11' : 'after:left-0'} pb-20 after:transition-all after:duration-500 w-20 h-8  rounded-full px-1 flex items-center after:rounded-full after:content-[""] after:flex after:relative after:w-7 after:h-7`}
       >
-         {isChecked ? <MoonSVG /> : <SunSVG />}
+         {isChecked ? <SunSVG /> : <MoonSVG /> }
       </div>
     </label>
   );
